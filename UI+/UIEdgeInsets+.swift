00cf70eee57704c569708a44a26ab3f053bc9f9b
Steve <chengquan.wang@ele.me>
2017-07-18 10:00:26 +0800

    add some property

--- UI+/UIEdgeInsets+.swift
+++ UI+/UIEdgeInsets+.swift
@@ -8,44 +8,47 @@
 
 import UIKit
 
-extension UIEdgeInsets {
-    public init(all: CGFloat) {
+public extension UIEdgeInsets {
+
+    init(all: CGFloat) {
         self.init(top: all, left: all, bottom: all, right: all)
     }
 
-    public init(topBottom: CGFloat, leftRight: CGFloat) {
+    init(topBottom: CGFloat, leftRight: CGFloat) {
         self.init(top: topBottom, left: leftRight, bottom: topBottom, right: leftRight)
     }
 
-    public init(topBottom: CGFloat) {
+    init(topBottom: CGFloat) {
         self.init(top: topBottom, left: 0, bottom: topBottom, right: 0)
     }
 
-    public init(leftRight: CGFloat) {
+    init(leftRight: CGFloat) {
         self.init(top: 0, left: leftRight, bottom: 0, right: leftRight)
     }
 
-    public init(left: CGFloat) {
+    init(left: CGFloat) {
         self.init(top: 0, left: left, bottom: 0, right: 0)
     }
 
-    public init(bottom: CGFloat) {
+    init(bottom: CGFloat) {
         self.init(top: 0, left: 0, bottom: bottom, right: 0)
     }
 
-    public init(right: CGFloat) {
+    init(right: CGFloat) {
         self.init(top: 0, left: 0, bottom: 0, right: right)
     }
 
-    public init(top: CGFloat) {
+    init(top: CGFloat) {
         self.init(top: top, left: 0, bottom: 0, right: 0)
     }
 
-    public var topBottom: (CGFloat, CGFloat) {
+    var topBottom: (CGFloat, CGFloat) {
         return (self.top, self.bottom)
     }
     
-    public var leftRight: (CGFloat, CGFloat) {
+    var leftRight: (CGFloat, CGFloat) {
         return (self.left, self.right)
     }
+
+
 }
--- UI+/UIScrollView+.swift
+++ UI+/UIScrollView+.swift
@@ -10,6 +10,24 @@ import UIKit
 
 public extension UIScrollView {
 
+    /// `set` `get` the content's offsetX
+    var offsetX: CGFloat {
+        get {
+            return contentOffset.x
+        } set {
+            contentOffset = CGPoint(x: newValue, y: contentOffset.y)
+        }
+    }
+
+    /// `set` `get` the content's offsetY
+    var offsetY: CGFloat {
+        get {
+            return contentOffset.y
+        } set {
+            contentOffset = CGPoint(x: contentOffset.x, y: newValue)
+        }
+    }
+
     /// `set` `get` the content's height
     var contentHeight: CGFloat {
         get {
@@ -28,21 +46,41 @@ public extension UIScrollView {
         }
     }
 
-    /// `set` `get` the content's offsetX
-    var offsetX: CGFloat {
+
+    /// `set` `get` the contentInset's top
+    var contentInsetTop: CGFloat {
         get {
-            return contentOffset.x
+            return contentInset.top
         } set {
-            contentOffset = CGPoint(x: newValue, y: contentOffset.y)
+            contentInset = UIEdgeInsets(top: newValue, left: contentInset.left, bottom: contentInset.bottom, right: contentInset.right)
         }
     }
 
-    /// `set` `get` the content's offsetY
-    var offsetY: CGFloat {
+    /// `set` `get` the contentInset's bottom
+    var contentInsetBottom: CGFloat {
         get {
-            return contentOffset.y
+            return contentInset.bottom
         } set {
-            contentOffset = CGPoint(x: contentOffset.x, y: newValue)
+            contentInset = UIEdgeInsets(top: contentInset.top, left: contentInset.left, bottom: newValue, right: contentInset.right)
         }
     }
+
+    /// `set` `get` the contentInset's left
+    var contentInsetLeft: CGFloat {
+        get {
+            return contentInset.left
+        } set {
+            contentInset = UIEdgeInsets(top: contentInset.top, left: newValue, bottom: contentInset.bottom, right: contentInset.right)
+        }
+    }
+
+    /// `set` `get` the contentInset's top
+    var contentInsetRight: CGFloat {
+        get {
+            return contentInset.right
+        } set {
+            contentInset = UIEdgeInsets(top: top, left: contentInset.left, bottom: contentInset.bottom, right: newValue)
+        }
+    }
+
 }
