2411dc597d5c41d977b59dee86550ad00d182d99
steve <chengquan.wang@ele.me>
2018-09-17 20:40:23 +0800

    gradientBackground for UIView

--- UI+/UIView+.swift
+++ UI+/UIView+.swift
@@ -308,3 +308,32 @@ public extension UIView {
         }
     }
 }
+
+enum GradientDirection {
+    case leftToRight
+    case rightToLeft
+    case topToBottom
+    case bottomToTop
+}
+
+extension UIView {
+    func gradientBackground(colors: [UIColor], direction: GradientDirection) {
+        let gradient = CAGradientLayer()
+        gradient.frame = self.bounds
+        gradient.colors = colors.map{$0.cgColor}
+        switch direction {
+        case .leftToRight:
+            gradient.startPoint = CGPoint(x: 0.0, y: 0.5)
+            gradient.endPoint = CGPoint(x: 1.0, y: 0.5)
+        case .rightToLeft:
+            gradient.startPoint = CGPoint(x: 1.0, y: 0.5)
+            gradient.endPoint = CGPoint(x: 0.0, y: 0.5)
+        case .bottomToTop:
+            gradient.startPoint = CGPoint(x: 0.5, y: 1.0)
+            gradient.endPoint = CGPoint(x: 0.5, y: 0.0)
+        default:
+            break
+        }
+        self.layer.insertSublayer(gradient, at: 0)
+    }
+}
