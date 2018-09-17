af3dd5269fbd38280e3bffc9774627e4c1d91c4b
steve <chengquan.wang@ele.me>
2018-09-17 20:27:12 +0800

    CALayer +

---
+++ UI+/CALayer+.swift
@@ -0,0 +1,30 @@
+//
+//  CALayer+.swift
+//  Pods
+//
+//  Created by steve on 17/09/2018.
+//
+
+
+extension CALayer {
+    func applySketchShadow(
+        color: UIColor = .black,
+        alpha: Float = 0.5,
+        x: CGFloat = 0,
+        y: CGFloat = 2,
+        blur: CGFloat = 4,
+        spread: CGFloat = 0)
+    {
+        shadowColor = color.cgColor
+        shadowOpacity = alpha
+        shadowOffset = CGSize(width: x, height: y)
+        shadowRadius = blur / 2.0
+        if spread == 0 {
+            shadowPath = nil
+        } else {
+            let dx = -spread
+            let rect = bounds.insetBy(dx: dx, dy: dx)
+            shadowPath = UIBezierPath(rect: rect).cgPath
+        }
+    }
+}
